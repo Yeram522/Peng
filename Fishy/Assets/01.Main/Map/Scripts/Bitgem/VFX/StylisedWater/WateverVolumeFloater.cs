f232e3f4a4fd357f4a47176476cc38ce8fc4f376
Yeram522 <kyeram522@gmail.com>
2021-10-08 23:53:27 +0900

    Player Control
    ✔ 키보드입력으로 이동 및 점프 구현
    + Animation mechnism 구현
    ✔ Tutorial Scene 생성.

--- Fishy/Assets/01.Main/Map/Scripts/Bitgem/VFX/StylisedWater/WateverVolumeFloater.cs
+++ Fishy/Assets/01.Main/Map/Scripts/Bitgem/VFX/StylisedWater/WateverVolumeFloater.cs
@@ -1,4 +1,4 @@
-癤�#region Using statements
+#region Using statements
 
 using System.Collections;
 using System.Collections.Generic;
@@ -17,17 +17,37 @@ namespace Bitgem.VFX.StylisedWater
         #endregion
 
         #region MonoBehaviour events
-
         void Update()
         {
+            
             var instance = WaterVolumeHelper ? WaterVolumeHelper : WaterVolumeHelper.Instance;
             if (!instance)
             {
+                
                 return;
             }
-
+            if(Input.anyKey) return;
+            
+            
             transform.position = new Vector3(transform.position.x, instance.GetHeight(transform.position) ?? transform.position.y, transform.position.z);
         }
+
+        private void OnTriggerEnter(Collider other)//충돌하는 순간
+        {
+            if(other.tag != "water") return;
+            Debug.Log("swimming");
+            GetComponent<Animator>().SetBool("isMoving",false);
+            GetComponent<Animator>().SetBool("isInWater",true);
+        }
+
+        private void OnTriggerExir(Collider other)//충돌하는 순간
+        {
+            if(other.tag != "water") return;
+            //Debug.Log("swimming");
+            GetComponent<Animator>().SetBool("isInWater",false);
+        }
+
+      
 
         #endregion
     }
