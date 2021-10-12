c28df251db7b57efdf9aa3ef30a3cd4fc82daee4
Yeram522 <kyeram522@gmail.com>
2021-10-13 01:11:56 +0900

    맵 동적생성 및 UI 디자인
    ✔ UI(50%)
    ✔ 발판 이동 문제 해결하기.(완료)
    ✔ GameOver, Clear 조건 달성(완료)
    ✔ Spawner 동적 생성(완료)

--- Fishy/Assets/01.Main/Script/GameManager.cs
+++ Fishy/Assets/01.Main/Script/GameManager.cs
@@ -2,34 +2,109 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameState
+{
+    inGame,
+    gameOver
+}
+
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    public GameState currentState = GameState.inGame;
+
     public GameObject StartPoint;
     public GameObject EndPoint;
-
     public GameObject Player;
+    public GameObject UI;
     
-    public GameObject SpawnerPrefab;
+    public GameObject SpawnerPrefab;//나중에 Asset에서 가져오기.
 
-    private Transform SpawnPosition; //Sapwner들이 동적으로 생성될 position
+   // public GameObject SpawnPosition; //Sapwner들이 동적으로 생성될 position
 
-    void Start()
+    private GameObject spawned;//Instanciate 정보를 담을 오브젝트
+
+    public bool gameOver;
+
+
+    void Awake()
     {
-        
+        instance = this;//GameManager 싱글톤
+
+        //제어할 게임 오브젝트 찾기.
+        StartPoint = transform.Find("StartPoint").gameObject;
+        EndPoint  = transform.Find("EndPoint").gameObject;
+        Player = transform.Find("Penguin").gameObject;
+
+        //SpawnPosition.transform.position = SpawnerPrefab.transform.position;//레벨 스포너를 생성할 위치의 기준 점을 저장.
+
+        StartCoroutine("StartGame");
     }
 
     void Update()
     {
+        if(!gameOver) return;
+        //gameOver이 true이면
+        StopCoroutine("StartGame");
+    }
+
+    IEnumerator StartGame()
+    {
+        currentState = GameState.inGame;
+
+        StartCoroutine("dynamicSpawnLevel");
+
+        //동적 레벨 생성
+        yield return null;  
+    }
+
+    public void GameClear()
+    {
+        Debug.Log("게임클리어");
+        //Clear UI 나오기.
+    }
+
+    public void GameOver()
+    {
+        currentState = GameState.gameOver;
+        StopCoroutine("StartGame");
+        //UI처리
+        Debug.Log("게임오버");
+    }
+
+    public void StopSpawnLevel()
+    {
+        StopCoroutine("dynamicSpawnLevel");
         
     }
 
+    //동적으로 스포너 생성.
     IEnumerator dynamicSpawnLevel()
-    {         
+    {
+        int count = 1;         
         while(true)
         {
-         
-         yield return new WaitForSeconds(5.0f);
-        }
-        
+            Vector3 pos = new Vector3(SpawnerPrefab.transform.position.x,SpawnerPrefab.transform.position.y,SpawnerPrefab.transform.position.z+2.0f*count);//매 텀마다 z값이 점점 축적 되면서 값이 증가한다.
+            int rand = Random.Range(0, 2);
+            if(rand==0)
+            {
+                spawned = Instantiate(SpawnerPrefab , pos , Quaternion.Euler(new Vector3(0,180.0f,0)));
+            }
+            else if(rand==1)
+            {
+                spawned = Instantiate(SpawnerPrefab , pos , Quaternion.Euler(0,0,0));
+            }
+            //scale 조절
+            Vector3 originScale = spawned.transform.localScale;
+            spawned.transform.SetParent(transform);
+            spawned.transform.localScale = originScale;
+
+            count ++;
+                   
+            yield return new WaitForSeconds(3.0f);
+        }    
+
+        yield return null;  
     }
+  
 }
--- Fishy/Assets/01.Main/Script/Spawner.cs
+++ Fishy/Assets/01.Main/Script/Spawner.cs
@@ -11,17 +11,22 @@ public class Spawner : MonoBehaviour
     private float interval;
     private GameObject spawned;
     private GameObject spawnedEnemy;
-    private Vector3 spawnBirdPos;
+    //private Vector3 spawnBirdPos;
     private bool delaytrigger=false; 
-    public GameManager Manager;
 
     void Start()
     {
-        Manager = transform.parent.Find("GameManager").GetComponent<GameManager>();   
-        interval = Random.Range(2.5f, 4.0f);
-        spawnBirdPos = new Vector3(transform.position.x,transform.position.y+0.2f,transform.position.z);
+        
+        //spawnBirdPos = new Vector3(transform.position.x,transform.position.y,transform.position.z);
         StartCoroutine(sponStep());      
         StartCoroutine(sponEnemy());
+    }
+
+    //동적생성 제어.
+    private void OnTriggerEnter(Collider other)//맵 끝과 충돌하면 코루틴을 멈춘다.
+    {
+        if(other.tag != "Finish") return;
+        GameManager.instance.StopCoroutine("dynamicSpawnLevel");
     }
 
     //발판을 스폰한다.
@@ -29,6 +34,7 @@ public class Spawner : MonoBehaviour
     {
         while(true)
         {
+         interval = Random.Range(2.3f,2.8f);
          int rand = Random.Range(1, 4);
          switch(rand)
          {
@@ -54,14 +60,14 @@ public class Spawner : MonoBehaviour
     {         
         while(true)
         {
-         float randtime = Random.Range(5.0f , 10.0f);
+         float randtime = Random.Range(20.0f , 30.0f);
          if(delaytrigger == false) 
          {
             delaytrigger=true;
-            yield return new WaitForSeconds(4.0f);
+            yield return new WaitForSeconds(5.0f);
             continue;
          }
-         GameObject spawnedEnemy = Instantiate(enemy , spawnBirdPos , enemy.transform.rotation);
+         GameObject spawnedEnemy = Instantiate(enemy , transform.position , enemy.transform.rotation);
          Vector3 originScale = spawnedEnemy.transform.localScale;
          spawnedEnemy.transform.SetParent(transform.parent);
          spawnedEnemy.transform.localScale = originScale;
